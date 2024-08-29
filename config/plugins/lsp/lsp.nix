21fa9d535c0618df675bcfee1afe8f38e9ccdcea
Patrick <fwy996602672@gmail.com>
2024-08-29 18:37:56 +0800

    use hls in lsp

--- config/plugins/lsp/lsp.nix
+++ config/plugins/lsp/lsp.nix
@@ -7,7 +7,7 @@
       enable = true;
       inlayHints = true;
       servers = {
-        haskell = {enable = true;};
+        hls = {enable = true;};
         html = {enable = true;};
         lua-ls = {enable = true;};
         nil-ls = {enable = true;};
